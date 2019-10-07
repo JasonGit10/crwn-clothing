61fce9ca2f87efdb20843c1d1b3c3d46ace26342
jportilla <jportilla@proveedor.teco.com.ar>
2019-10-07 18:16:43 -0300

    Agrego componente SignUp

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,32 @@ const config = {
     appId: "1:523043551697:web:0b66bc6b3884aaa53dc8f0"
   };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if(!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists) {
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error){
+      console.log('error creating user',error.message);
+    }
+  }
+
+  return userRef;
+}
+
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
