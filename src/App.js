a398e9e1a87cb3412d779e05499168b4cf0992c6
JPortilla <jportilla@proveedor.teco.com.ar>
2019-10-27 03:21:04 -0300

    Redirecciono a home si hay un usuario logeado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch,Route} from 'react-router-dom';
+import {Switch,Route,Redirect} from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
@@ -45,16 +45,20 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component = {Homepage}></Route>
           <Route exact path='/shop' component = {ShopPage}></Route>
-          <Route exact path='/signin' component = {SignInAndSignUpPage}></Route>
+          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage/>)}></Route>
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user}) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
   });
 
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
